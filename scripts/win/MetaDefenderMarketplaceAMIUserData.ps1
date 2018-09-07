b45805d9053d378bad5be2228b4a4d6fa9eee4a3
George Prichici <georgeprichici@users.noreply.github.com>
2018-09-07 15:16:39 -0700

    Create user admin/instance-id and bypass the welcome wizard.

--- scripts/win/MetaDefenderMarketplaceAMIUserData.ps1
+++ scripts/win/MetaDefenderMarketplaceAMIUserData.ps1
@@ -1,94 +1,33 @@
-<powershell>
-$lock = "C:\Program Files\OPSWAT\pwd_ch_lock"
-$restUrl = "http://localhost:8008"
-
-function MetaDefenderLogin() {
+$restIP = "localhost"
+$restPort = 8008
+$restUrl = "http://" + $restIP + ":" + $restPort
+function MetaDefenderCreateUser() {
     Param
     (
         [Parameter(Mandatory=$true, Position=0)]
         [string] $username,
         [Parameter(Mandatory=$true, Position=1)]
-        [string] $password
+        [string] $password, 
+	    [Parameter(Mandatory=$true, Position=2)]
+	    [string] $apikey
     )
     $body = @{
-        "user"="$username";
+	    "directory_id"="1";
+        "name"="$username";
+	    "display_name"="$username";
+	    "email"="$username@local.com";
+	    "roles"=@("1");
         "password"="$password";
     } | ConvertTo-Json
 
-    $loginUrl = $restUrl + "/login"
-    $response = Invoke-WebRequest -UseBasicParsing -Uri $loginUrl -Method POST -Body $body -ContentType "application/json" | ConvertFrom-Json    
-    return $response.session_id    
-}
-
-function MetaDefenderChangePassword() {
-    Param
-    (
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $apikey         
-    )
-
-    #Set session id as apikey header
     $headers = @{
-        "apikey"="$apikey";
-    }
-        
-    #Create new password body
-    $body = @{
-        "old_password"="admin";
-        "new_password"="$instance";
-    } | ConvertTo-Json
-    
-    $changePassUrl = $restUrl + "/user/changepassword"
+	    "apikey"="$apikey";
+    } 
 
-    #Change password
-    return Invoke-WebRequest -UseBasicParsing -Uri $changePassUrl -Method POST -Headers $headers -Body $body -ContentType "application/json" | 
-            ConvertFrom-Json    
+    $createUserUrl = $restUrl + "/admin/user"    
+    Invoke-WebRequest -UseBasicParsing -Uri $createUserUrl -Method POST -Body $body -Headers $headers -ContentType "application/json" | ConvertFrom-Json
 }
 
-function AWSGetInstanceId() {
-    #Get instance name
-    return Invoke-WebRequest -UseBasicParsing -Uri http://169.254.169.254/latest/meta-data/instance-id -Method GET         
-}
-
-function HandleException() {
-    Param
-    (
-         [Parameter(Mandatory=$true, Position=0)]
-         [System.Exception] $exception   
-    )
-
-    $resp = $exception.Response
-
-    if ($resp -eq $null)
-    {
-        Write-host $exception
-    }
-    else
-    {
-        $responseStream = $resp.GetResponseStream()
-        $streamReader = new-object System.IO.StreamReader $responseStream
-        $errorStackTrace = $streamReader.ReadToEnd() 
-        $errorMessage = [string]$Exception.Message
-        $statusCode = $Exception.Response.StatusCode.Value__
-        $errorMessage = "Unable to activate license $ActivationKey :`nReason: [$statusCode] $errorMessage`n$errorStackTrace" 
-        Write-Error -message $errorMessage -exception $exception 
-    }
-}
-
-$instance = AWSGetInstanceId
-
-#Test for lock
-if (Test-Path $lock) {
-    
-    $credentials = @{
-        "user"="admin";
-        "password"="$instance";
-    }
-    $shouldChangePassword = $false
-} else {
-    $credentials = @{ "user"="admin"; "password"="admin";}
-    $shouldChangePassword = $true
-}
 
 #check if MetaDefender service is up
 while (-not (Test-NetConnection localhost -Port 8008 | ? { $_.TcpTestSucceeded } )) {
@@ -97,20 +36,52 @@ while (-not (Test-NetConnection localhost -Port 8008 | ? { $_.TcpTestSucceeded }
 }
 
 Try
-{           
-    $sessionId = MetaDefenderLogin -username $credentials.user -password $credentials.password
-
-    if ($shouldChangePassword) {
-        $response = MetaDefenderChangePassword  -apikey $sessionId
-        if ($response) {
-            #Create passwd change lock
-            New-Item "$lock" -ItemType file
-        }
+{
+    $indexHTML = $restURL + "/index.html"
+    $wizardHTML = Invoke-WebRequest -UseBasicParsing -Uri $indexHTML -Method GET
+    $found = $wizardHTML.content -match "wizard\..*\.js"   
+    if (-not $found){
+        return $false
+    }    
+    
+    $wizardJSURL = $restURL + "/assets/js/" + $matches[0]
+    echo "Wizard Location: $wizardJSURL"
+    $wizardJ = Invoke-WebRequest -UseBasicParsing -Uri $wizardJSURL -Method GET 
+    $found = $wizardJ -match 'setSessionId\("[a-zA-Z0-9]+'
+    if (-not $found) {
+        return $false
     }
+    $defaultAPIKey = $matches[0] -replace 'setSessionId\("', ""    
+    $instance = AWSGetInstanceId
+    # $instance = "admin"
+    
+    MetaDefenderCreateUser -username "admin" -password "$instance" -apikey "$defaultAPIKey"
+    
+    $headers = @{
+        "apikey"="$defaultAPIKey";
+    }
+
+    $welcomeURL = $restURL + "/admin/welcome"    
+    Invoke-WebRequest -UseBasicParsing -Uri $welcomeURL -Method POST -Headers $headers
 }
 Catch
 {
-    HandleException -exception $_.Exception 
+    # HandleException -exception $_.Exception 
+    $exception = $_.Exception
+    $resp = $exception.Response
+
+    if ($resp -eq $null)
+    {
+        Write-host $_.Exception
+    }
+    else
+    {
+        $responseStream = $resp.GetResponseStream()
+        $streamReader = new-object System.IO.StreamReader $responseStream
+        $errorStackTrace = $streamReader.ReadToEnd() 
+        $errorMessage = [string]$Exception.Message
+        $statusCode = $Exception.Response.StatusCode.Value__
+        $errorMessage = "Failed :`nReason: [$statusCode] $errorMessage`n$errorStackTrace" 
+        Write-Error -message $errorMessage -exception $exception 
+    }
 }
-</powershell>
-<persist>true</persist>
